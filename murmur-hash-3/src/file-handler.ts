051af887c949dd3bac626d98b3789fc94a1e11f2
misxka <mishutkavlesu@gmail.com>
2022-03-08 14:49:51 +0300

    feat: add ability to calculate hash value for files

---
+++ murmur-hash-3/src/file-handler.ts
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import { Transform } from 'stream';
+import { MurmurHash3 } from './murmur-hash-3';
+
+type Options = {
+  inputFilename: string;
+  seed: number;
+};
+
+export const calculateFileHash = (options: Options) => {
+  const { inputFilename, seed } = options;
+
+  const fileContent = fs.readFileSync(inputFilename, { encoding: 'utf-8' });
+
+  const murmurHash3 = new MurmurHash3(seed);
+  return murmurHash3.hash(fileContent);
+};
--- murmur-hash-3/src/index.ts
+++ murmur-hash-3/src/index.ts
@@ -1,18 +1,37 @@
 #!/usr/bin/env node
 
+import path from 'path';
 import yargs from 'yargs';
 import chalk from 'chalk';
 
 import { MurmurHash3 } from './murmur-hash-3';
+import { calculateFileHash } from './file-handler';
+
+const SourceTypes = {
+  file: 'file',
+  cli: 'cli'
+};
 
 const options = yargs
   .options({
-    v: { describe: 'value', type: 'string', demandOption: true },
-    s: { describe: 'seed', type: 'number', demandOption: true }
+    v: { describe: "value (needed when 'cli' source type is selected)", type: 'string' },
+    s: { describe: 'seed', type: 'number', demandOption: true },
+    t: { describe: 'source type', choices: ['file', 'cli'] as const, type: 'string', demandOption: true },
+    p: { alias: 'path', describe: "Path to file where the data is stored (ignored when 'cli' source option selected)", type: 'string' }
   })
   .parseSync();
 
-const { v: input, s: seed } = options;
+const { v: input, s: seed, t: sourceType, p: inputFilename } = options;
 
-const murmurHash3 = new MurmurHash3(seed);
-console.log(chalk.green(`Result: ${murmurHash3.hash(input).toString(16)}`));
+let result: number;
+switch (sourceType) {
+  case SourceTypes.cli:
+    const murmurHash3 = new MurmurHash3(seed);
+    result = murmurHash3.hash(input);
+    break;
+  case SourceTypes.file:
+    result = calculateFileHash({ inputFilename: path.join(__dirname, inputFilename), seed });
+    break;
+}
+
+console.log(chalk.green(`Result: ${result.toString(16)}`));
